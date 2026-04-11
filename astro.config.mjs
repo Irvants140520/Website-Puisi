c7a77ddb7ab807d233141ae8eb44d9967b46a991
Irvants140520 <139057356+Irvants140520@users.noreply.github.com>
2026-04-11 22:22:45 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,5 @@ import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   output: 'server',
-  adapter: netlify({
-    edgeMiddleware: false,
-    functionPerRoute: false,
-  }),
-  vite: {
-    define: {
-      // Paksa Netlify dev tidak load edge runtime
-      'process.env.NETLIFY_LOCAL': '"true"',
-    },
-  },
+  adapter: netlify(),
 });
